798d7ffd43b5c77f9f1cd492abe9ff87bd3a5e50
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2020-07-07 11:20:15 +0530

    Update app.component.ts
    added changes as per application

--- customerApplication/src/app/app.component.ts
+++ customerApplication/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Customer } from './app.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.view.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class CustomerComponent {
   title = 'customerApplication';
+  customerMode: Customer = new Customer();
 }
