8f0c7bb1bdcb38ed0a3a3292090a2fe1ea77f3e8
pranay <pranayghiya222@gmail.com>
2020-07-17 23:06:54 +0530

    added validations to formGroup

--- customerApplication/src/CustomerApp/customer/customer.model.ts
+++ customerApplication/src/CustomerApp/customer/customer.model.ts
@@ -1,5 +1,18 @@
+import { NgForm, FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms'
+
 export class Customer {
     customerCode = '';
     customerName = '';
     customerAmount = 0;
+    formGroup: FormGroup = null;
+
+    constructor(){
+        let _builder = new FormBuilder();
+        this.formGroup = _builder.group({});
+        this.formGroup.addControl("CustomerNameControl", new FormControl('', Validators.required));
+
+        let validationCollection = [];
+        validationCollection.push(Validators.required, Validators.pattern("^[0-9]{4,4}$"));
+        this.formGroup.addControl("CutomerCodeControl", new FormControl('', Validators.compose(validationCollection)));
+    }
 }
