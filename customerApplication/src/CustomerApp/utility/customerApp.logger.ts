98b6819d0953667c783e7285e61454ad1fdd75c9
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2020-07-23 15:50:56 +0530

    Added changes for logger

--- customerApplication/src/CustomerApp/utility/customerApp.logger.ts
+++ customerApplication/src/CustomerApp/utility/customerApp.logger.ts
@@ -1,1 +1,19 @@
-//Logger code
+export interface Logger {
+    Log();
+}
+
+export class BaseLogger implements Logger{
+    Log(){}
+}
+
+export class ConsoleLogger extends BaseLogger{
+    Log(){
+        console.log('Console Logger');
+    }
+}
+
+export class DBLogger extends BaseLogger{
+    Log(){
+        console.log('DB Logger');
+    }
+}
